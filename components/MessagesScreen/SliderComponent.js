9321d1e76738d031f74b0dadc4215334d69585f6
johan030 <weylin030@protonmail.ch>
2022-05-13 15:39:21 +0200

    fixing image slider styles

--- components/MessagesScreen/SliderComponent.js
+++ components/MessagesScreen/SliderComponent.js
@@ -1,48 +1,63 @@
-import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image, StyleSheet } from "react-native";
+import React from "react";
 import { IMAGES } from "../../data/Images";
 
 const SliderComponent = () => {
-
   return (
     <>
       <View>
-        <Text style={{ color: 'brown', letterSpacing: 1, marginLeft: '1rem', marginTop: '.5rem', marginBottom: '.5rem' }}>Sorties et Groupes</Text>
-
+        <Text
+          style={{
+            color: "#a56210",
+            letterSpacing: 1,
+            marginLeft: "1rem",
+            marginTop: ".5rem",
+            marginBottom: ".5rem",
+          }}
+        >
+          Sorties et Groupes
+        </Text>
       </View>
       <View>
         <ScrollView horizontal persitentScrollbar={true}>
           {IMAGES.map((img, index) => (
-            <View key={index} tyle={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Image source={{ uri: img.image }} style={styles.imgStyle}
-              />
+            <View key={index}>
+              <Image source={{ uri: img.image }} style={styles.imgStyle} />
               <Text style={styles.sliderText}>{img.name}</Text>
-            </View> 
+            </View>
           ))}
         </ScrollView>
       </View>
     </>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   imgStyle: {
-    width: 90,
-    height: 90,
-    borderRadius: 15,
-    marginLeft: 6,
-    
+    width: 110,
+    height: 110,
+    borderRadius: 10,
+    marginTop: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    position: "relative",
   },
-  sliderText:{
-    position: 'absolute',
+  sliderText: {
+    fontSize: 15,
+    fontWeight: '500',
+    position: "absolute",
     bottom: 0,
-    backgroundColor: 'gray',
-    color: 'white',
-    margin: 'auto 0'
-    
+    right: 5,
+    top: "70%",
+    left: 5,
+    textAlign: "center",
+    backgroundColor: "#60605f",
+    color: "white",
+    borderRadius: 5,
+    zIndex: 1000,
+    resizeMode: 'contain'
+  },
+});
 
-
-  }
-})
-
-export default SliderComponent
+export default SliderComponent;
